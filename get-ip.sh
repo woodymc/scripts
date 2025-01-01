5abbe03c92dce1b5b2d7e2568f23a12dd2fb4f0e
IGOR MARKIN <39812060+woodymc@users.noreply.github.com>
2025-01-01 14:35:15 +0300

    Update get-ip.sh

--- get-ip.sh
+++ get-ip.sh
@@ -12,9 +12,9 @@ for host in ${IDENTHOST}; do
         avg=$(echo "$resp" | awk -F'[/=]' 'END{print $6}')
         loss=$(echo "$resp" | awk '/packet loss/ {print $7}' | tr -d '%')
         if [ "$loss" == 100 ]; then
-                printf "║ \033[34;1m$host\033[0m\x09IP: \033[32;1m$geo\033[0m@$ip\x09ping: \033[31;1mNot response\033[0m\x09\033[33;1mloss: $loss\x25\033[0m\x09║\n"
+                printf "║ \033[34;1m$host\033[0m\x09IP: \033[32;1m$geo\033[0m|$ip\x09ping: \033[31;1mNot response\033[0m\x09\033[33;1mloss: $loss\x25\033[0m\x09║\n"
         else
-                printf "║ \033[34;1m$host\033[0m\x09IP: \033[32;1m$geo\033[0m@$ip\x09ping: $avg(AVG)\x09\033[33;1mloss: $loss\x25\033[0m\x09║\n"
+                printf "║ \033[34;1m$host\033[0m\x09IP: \033[32;1m$geo\033[0m|$ip\x09ping: $avg(AVG)\x09\033[33;1mloss: $loss\x25\033[0m\x09║\n"
         fi
 done
 printf "║                                                                               ║\n"

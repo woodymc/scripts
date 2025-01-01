f688a8c104e01b4e067545a1a14c7087f979a004
IGOR MARKIN <39812060+woodymc@users.noreply.github.com>
2025-01-01 14:36:22 +0300

    Update get-ip.sh

--- get-ip.sh
+++ get-ip.sh
@@ -7,10 +7,10 @@ printf "╔═══════════════════════
 printf "║                                                                               ║\n"
 for host in ${IDENTHOST}; do
         ip=$(curl -s --interface tun0 $host)
-        geo=$(curl -s "https://get.geojs.io/v1/ip/country.json?ip=$ip" | jq -r ".[0].country")
         resp=$(ping -qc$COUNT "$ip")
         avg=$(echo "$resp" | awk -F'[/=]' 'END{print $6}')
         loss=$(echo "$resp" | awk '/packet loss/ {print $7}' | tr -d '%')
+        geo=$(curl -s "https://get.geojs.io/v1/ip/country.json?ip=$ip" | jq -r ".[0].country")
         if [ "$loss" == 100 ]; then
                 printf "║ \033[34;1m$host\033[0m\x09IP: \033[32;1m$geo\033[0m|$ip\x09ping: \033[31;1mNot response\033[0m\x09\033[33;1mloss: $loss\x25\033[0m\x09║\n"
         else

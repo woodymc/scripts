924580ec2af6b043e3b51d60be9cda18022fd26f
IGOR MARKIN <39812060+woodymc@users.noreply.github.com>
2025-01-01 14:21:24 +0300

    Update get-ip.sh

--- get-ip.sh
+++ get-ip.sh
@@ -6,14 +6,15 @@ COUNT=10
 printf "╔══════════════════════════════════ \033[32;1mYour IP's\033[0m ══════════════════════════════════╗\n"
 printf "║                                                                               ║\n"
 for host in ${IDENTHOST}; do
-        ip="$(curl -s --interface tun0 $host)"
+        ip=$(curl -s --interface tun0 $host)
+        geo=$(curl -s "https://get.geojs.io/v1/ip/country.json?ip=$ip" | jq -r ".[0].country")
         resp=$(ping -qc$COUNT "$ip")
         avg=$(echo "$resp" | awk -F'[/=]' 'END{print $6}')
         loss=$(echo "$resp" | awk '/packet loss/ {print $7}' | tr -d '%')
         if [ "$loss" == 100 ]; then
-                printf "║ $host\x09IP: $ip\x09ping: Not response\x09loss: $loss\x25\x09║\n"
+                printf "║ $host\x09$geo: $ip\x09ping: Not response\x09loss: $loss\x25\x09║\n"
         else
-                printf "║ $host\x09IP: $ip\x09ping: $avg(AVG)\x09loss: $loss\x25\x09║\n"
+                printf "║ $host\x09$geo: $ip\x09ping: $avg(AVG)\x09loss: $loss\x25\x09║\n"
         fi
 done
 printf "║                                                                               ║\n"

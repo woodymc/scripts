af958428228fc933d3044a4a254dd323c237a948
IGOR MARKIN <39812060+woodymc@users.noreply.github.com>
2025-01-06 16:27:52 +0300

    Update get-ip.sh

--- get-ip.sh
+++ get-ip.sh
@@ -23,7 +23,7 @@ if [[ -n "$(ip a | grep $iface)" ]]; then
 		        if [ "$loss" == 100 ]; then
 		                printf "║ \033[34;1m$host\033[0m\x09IP: \033[32;1m$geo\033[0m|$ip\x09ping: \033[31;1mNot response\033[0m\x09\033[33;1mloss: $loss\x25\033[0m\x09║\n"
 		        else
-		                printf "║ \033[34;1m$host\033[0m\x09IP: \033[32;1m$geo\033[0m|$ip\x09ping: $avg(AVG)\x09\033[33;1mloss: $loss\x25\033[0m\x09║\n"
+		                printf "║ \033[34;1m$host\033[0m\x09IP: \033[32;1m$geo\033[0m|$ip\x09ping: $avg(AVG)\x09\033[33;1mloss: $loss\x25\033[0m\x09				║\n"
 		        fi
 	  	else
     			printf "║ \033[34;1m$host\033[0m\x09IP: \033[31;1mNOT RESOLV\033[31;1m\033[0m\x09║\n"

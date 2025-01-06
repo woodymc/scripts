aa351bfdb030246e24e3cdfd4031585f7529524d
IGOR MARKIN <39812060+woodymc@users.noreply.github.com>
2025-01-06 15:44:45 +0300

    Update get-ip.sh

--- get-ip.sh
+++ get-ip.sh
@@ -4,8 +4,9 @@ IDENTHOST='2ip.ru ifconfig.me showip.net 2ip.io'
 COUNT=10
 
 read -t 5 -p "Enter checked interface (timeout in 5 seconds):\n" riface
+
 if [[ -n "$riface" ]]; then
-  iface="$riface"
+  iface=$($riface)
 else
   iface="tun0"
 fi

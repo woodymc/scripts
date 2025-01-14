b811446c99836e2a7880b3b662aba1c21693742e
IGOR MARKIN <39812060+woodymc@users.noreply.github.com>
2025-01-15 00:52:21 +0300

    Update get-ip.sh

--- get-ip.sh
+++ get-ip.sh
@@ -19,8 +19,8 @@ if [[ -n "$1" ]]; then
 fi
 
 if ! $(opkg list-installed | grep -q jq); then
-	printf "!!!jq NOT installed";
-	opkg update -s
+	printf "###jq NOT installed";
+	opkg update -q
 fi
 
 

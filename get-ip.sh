232351c0032321df0f3fae2d6c9bc33c7da27755
IGOR MARKIN <39812060+woodymc@users.noreply.github.com>
2025-01-20 04:09:08 +0300

    Update get-ip.sh

--- get-ip.sh
+++ get-ip.sh
@@ -43,7 +43,7 @@ if [[ -n "$(ip a | grep $iface)" ]]; then
  	CheckProgramm 'jq'
 	printf "║										║\n"
 	for host in ${IDENTHOSTS}; do
-                ip=$(curl -si $iface $host)
+                ip=$(curl -sI $iface $host)
                 if [[ -n "$ip" ]]; then
 		        resp=$(ping -q -c $cnt -W 2 $ip)
 		        avg=$(echo "$resp" | awk -F'[/=]' 'END{print $6}')
